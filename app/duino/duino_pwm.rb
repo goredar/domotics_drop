9f88df1f6dd321655dd617a79ea148c68e4fb7e4
goredar <goredar@gmail.com>
2013-01-23 13:21:21 +0300

    new repo

--- app/duino/duino_pwm.rb
+++ app/duino/duino_pwm.rb
@@ -1,7 +1,11 @@
 #!/usr/bin/ruby -w
 # coding: utf-8
 
-class duino_led_strip
-  def initialize(board, room, args_hash)
+class DuinoPWM
+  def initialize(board, pin)
+    @board, @pin = board, pin
+  end
+  def level=(value)
+    @board.set_pwm(@pin, value)
   end
 end
---
+++ app/duino/duino_pwm/duino_led_strip.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby -w
+# coding: utf-8
+
+class DuinoLedStrip(board, room, args_hash)
+  def initialize()
+  end
+end
