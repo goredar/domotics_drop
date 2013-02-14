c51ae9ce55a0fdd5af72e34a0eda48258b0067e6
goredar <goredar@gmail.com>
2013-02-14 16:06:18 +0300

    Merge branch 'master' of github.com:goredar/domotics

--- web/app/helpers/application_helper.rb
+++ web/app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
-  def link_to_menu(label, path, deep = 1)
+  def link_to_menu(label, path, icon=nil, deep = 1)
     content_tag :li, :class => ('active' if '/' + request.path_info.split('/')[1,deep].join('/') == path) do
-      link_to label, path
+      content_tag :a, :href => path do
+        res = ''
+        if icon
+          icon, enclosed = icon.split
+          res += content_tag(:i, :class => "general#{'-enclosed' if enclosed} foundicon-#{icon}") {}
+        end
+        res += ' '+label
+        res.html_safe
+      end
     end
   end
 end
