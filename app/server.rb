132dac76f371271a2c143db56c3a2418023b96b9
goredar <goredar@gore-01.gore.local>
2013-03-05 22:26:31 +0300

    some fix

--- app/server.rb
+++ app/server.rb
@@ -39,7 +39,7 @@ module Domotics
       Socket.tcp_server_loop(SERVER_PORT) do |client, client_addrinfo|
         Thread.new do
           # client.puts GDS: Domotics::PROTOCOL_VERSION
-          $logger.info { "New client connection from #{client_addrinfo} accepted."
+          $logger.info { "New client connection from #{client_addrinfo} accepted." }
           loop do
             break if !message = client.gets
             begin
@@ -51,24 +51,17 @@ module Domotics
             # Evaluate expression in term of object
             # { request: :eval, object: :some_object, expression: :some_expression }
             when :eval
-              $logger.debug { data }
+              $logger.debug { data.inspect }
               $logger.debug state = Room[data[:object].to_sym].instance_eval(data[:expression])
               client.puts({ :state => state == :on ? :active : nil })
             when :get
-              client.puts '{response: :ok}'
             when :set
-              client.puts '{response: :ok}'
             when :script
-              client.puts '{response: :ok}'
             when :quit
-              client.puts '{response: :bye}'
               break
             when :test
-              client.puts '{response: :ok}'
             when :debug
-              client.puts '{response: :ok}'
             else
-              client.puts '{response: :unknown}'
               break
             end
           end
