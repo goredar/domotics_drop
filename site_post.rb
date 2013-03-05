cbcfda88680c297f27770e2e6dd8101a76ada694
goredar <goredar@gore-01.gore.local>
2013-03-05 22:26:31 +0300

    some fix

--- site_post.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby -w
-# coding: utf-8
-require 'net/http'
-require 'uri'
-
-while true do
-  Net::HTTP.post_form URI('http://127.0.0.1/main/post_terminal_line'), { :terminal_line => $stdin.readline }
-end
