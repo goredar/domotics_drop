a6667beb546639f0d4d56a69379323876a40cd87
goredar <goredar@gore-01.gore.local>
2013-03-05 22:26:31 +0300

    some fix

--- web/app/helpers/application_helper.rb
+++ web/app/helpers/application_helper.rb
@@ -2,13 +2,12 @@ module ApplicationHelper
   def link_to_menu(label, path, icon=nil, deep = 1)
     content_tag :li, :class => ('active' if '/' + request.path_info.split('/')[1,deep].join('/') == path) do
       content_tag :a, :href => path do
-        res = ''
         if icon
-          icon, enclosed = icon.split
-          res += content_tag(:i, :class => "general#{'-enclosed' if enclosed} foundicon-#{icon}") {}
+          selectors = icon.split.map { |sel| 'icon-'+sel }.join(' ')
+          content_tag(:i, :class => selectors) {} + label
+        else
+          label
         end
-        res += ' '+label
-        res.html_safe
       end
     end
   end
