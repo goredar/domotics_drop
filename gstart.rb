4297a0c963ef48f662c8b63d0b1b16e5884c36b6
goredar <goredar@gore-01.gore.local>
2013-03-05 22:26:31 +0300

    some fix

--- gstart.rb
+++ gstart.rb
@@ -4,6 +4,7 @@
 # Require subtree
 Dir[File.dirname(__FILE__) + '/app/**/*.rb'].sort.each {|file| require file}
 # Create logger
+require 'logger'
 logger = Logger.new(STDERR)
 logger.level = Logger::WARN
 # Run server
