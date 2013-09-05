c88e2955de306311258834bffdee896e62adccaf
Goredar <goredar@gmail.com>
2013-09-05 12:59:58 +0300

    gui update

--- web/js/script.js
+++ web/js/script.js
@@ -1,105 +1,138 @@
 (function() {
-  var DrawBlocks, PullElements, base_dim, col_count, gutter, gutter_part, row_count;
+  var rooms, view;
 
-  row_count = 4;
-
-  col_count = 6;
-
-  gutter_part = 0.04;
-
-  base_dim = 0;
-
-  gutter = 0;
-
-  DrawBlocks = function() {
-    var container_width, w_height, w_width;
-    w_width = $(window).width();
-    w_height = $(window).height();
-    base_dim = Math.round(w_height / (row_count * (1 + gutter_part) + gutter_part));
-    gutter = Math.round(base_dim * gutter_part);
-    $('body').height(w_height - gutter * 2).css('font-size', "" + (base_dim / 2) + "px");
-    container_width = (base_dim + gutter) * col_count;
-    $('.container').width(container_width);
-    $('.spacer').width(w_width - container_width - gutter * 2).css('margin_right', gutter).css('margin_left', gutter);
-    $('.column').width(base_dim + gutter);
-    $('.column-2x').width((base_dim + gutter) * 2);
-    $('.square').width(base_dim).height(base_dim).css('margin_bottom', gutter).css('margin_right', gutter);
-    $('.rectangle').width(base_dim).height(base_dim * 2 + gutter);
-    $('.square-1x').width(base_dim).height(base_dim).css('margin_right', gutter);
-    $('.square-2x').width(base_dim * 2 + gutter).height(base_dim * 2 + gutter);
-    $('.rectangle-land').width(base_dim * 2 + gutter).height(base_dim);
-    $('.rectangle-port').height(base_dim * 2 + gutter).width(base_dim).css('margin_right', gutter);
-    return $('.column, .column-2x').children().css('margin_bottom', gutter);
-  };
-
-  PullElements = function(data, status, xhr) {
-    if (status === "success") {
-      return $.each(data, function(room_name, room_data) {
-        return $.each(room_data["elements"], function(element_name, element_data) {
-          var element;
-          element = $("#" + room_name + " #" + element_name + " div");
-          element.removeClass();
-          element.addClass(element_data["state"]);
-          if (element_data["info"]) {
-            element = $("#" + room_name + " #" + element_name + " .info");
-            if (element.size() !== 0) {
-              return element.html(element_data["info"]);
+  view = {
+    row_count: 4,
+    col_count: 6,
+    gutter_part: 0.04,
+    init: function() {
+      $(".screen:not(:first-child)").hide();
+      window.onresize = function() {
+        return view.resize.call(view);
+      };
+      this.resize();
+      return this.hook();
+    },
+    resize: function() {
+      var base_dim, container_width, gutter, w_height, w_width;
+      w_width = $(window).width();
+      w_height = $(window).height();
+      base_dim = Math.round(w_height / (this.row_count + this.gutter_part * (this.row_count + 1)));
+      gutter = Math.round(base_dim * this.gutter_part);
+      container_width = (base_dim + gutter) * this.col_count;
+      $('body').height(w_height - gutter * 2).css('font-size', "" + (base_dim / 2) + "px");
+      $('.container').width(container_width);
+      $('.spacer').width(w_width - container_width - gutter * 2).css('margin_right', gutter).css('margin_left', gutter);
+      $('.column').width(base_dim + gutter);
+      $('.column-2x').width((base_dim + gutter) * 2);
+      $('.square').width(base_dim).height(base_dim).css('margin_right', gutter);
+      $('.rectangle').width(base_dim).height(base_dim * 2 + gutter);
+      $('.square-1x').width(base_dim).height(base_dim).css('margin_right', gutter);
+      $('.square-2x').width(base_dim * 2 + gutter).height(base_dim * 2 + gutter);
+      $('.rectangle-land').width(base_dim * 2 + gutter).height(base_dim);
+      $('.rectangle-port').height(base_dim * 2 + gutter).width(base_dim).css('margin_right', gutter);
+      return $("[class*=square], [class*=rectangle]").css('margin_bottom', gutter);
+    },
+    rotate: function(direction) {
+      return $(".screen").each(function() {
+        var next_screen;
+        if ($(this).is(":visible")) {
+          $(this).hide();
+          if (direction) {
+            next_screen = $(this).next(".screen");
+          } else {
+            next_screen = $(this).prev(".screen");
+          }
+          if (next_screen.size() === 1) {
+            next_screen.show();
+          } else {
+            if (direction) {
+              $(".screen").first().show();
             } else {
-              return $("#" + room_name + " #" + element_name).append("<span class='info'>" + element_data['info'] + "</span>");
+              $(".screen").last().show();
             }
           }
-        });
+          return false;
+        }
+        return true;
+      });
+    },
+    hook: function() {
+      var spacer;
+      $(document).on("click", "[data-command]", function() {
+        var request, screen;
+        screen = $(this).closest('.screen');
+        if (screen.size() === 1) {
+          request = "/" + ($(this).closest('.screen').attr('id'));
+        } else {
+          request = "";
+        }
+        if ($(this).attr("id")) {
+          request += "/" + ($(this).attr('id'));
+        }
+        request += "/" + ($(this).data('command'));
+        $.getJSON(request, rooms.update);
+        return false;
+      });
+      spacer = $(".spacer .room_switch");
+      $(document).on("click", spacer.first(), function() {
+        view.rotate(true);
+        return false;
+      });
+      $(document).on("click", spacer.last(), function() {
+        view.rotate(false);
+        return false;
+      });
+      return $(document).on("click", "[data-screen]", function() {
+        $(this).closest('.screen, .dialog').hide();
+        $("#" + ($(this).data('screen'))).show();
+        return false;
       });
     }
   };
 
-  $(".screen").each(function() {
-    return $.getJSON("" + ($(this).attr("id")) + "/state", PullElements);
-  });
-
-  $(document).on("click", "[data-command]", function() {
-    var request, screen;
-    screen = $(this).closest('.screen');
-    if (screen.size() === 1) {
-      request = "/" + ($(this).closest('.screen').attr('id'));
-    } else {
-      request = "";
-    }
-    if ($(this).attr("id")) {
-      request += "/" + ($(this).attr('id'));
-    }
-    request += "/" + ($(this).data('command'));
-    $.getJSON(request, PullElements);
-    return false;
-  });
-
-  $(document).on("click", ".spacer", function() {
-    $(".screen").each(function() {
-      var next_screen;
-      if ($(this).is(":visible")) {
-        $(this).hide();
-        next_screen = $(this).next(".screen");
-        if (next_screen.size() === 1) {
-          next_screen.show();
-        } else {
-          $(".screen").first().show();
+  rooms = {
+    init: function() {
+      return $(".screen").each(function() {
+        return $.getJSON("" + ($(this).attr("id")) + "/state", rooms.update);
+      });
+    },
+    update: function(data, status, xhr) {
+      var element, element_data, element_info, element_name, room_data, room_name, _results;
+      if (status === "success") {
+        _results = [];
+        for (room_name in data) {
+          room_data = data[room_name];
+          _results.push((function() {
+            var _ref, _results1;
+            _ref = room_data.elements;
+            _results1 = [];
+            for (element_name in _ref) {
+              element_data = _ref[element_name];
+              element = $("#" + room_name + " #" + element_name + " div");
+              element.removeClass();
+              element.addClass(element_data.state);
+              if (element_data.info) {
+                element_info = element.children(".info");
+                if (element_info.size() !== 0) {
+                  _results1.push(element_info.html(element_data.info));
+                } else {
+                  _results1.push(element.append("<span class='info'>" + element_data.info + "</span>"));
+                }
+              } else {
+                _results1.push(void 0);
+              }
+            }
+            return _results1;
+          })());
         }
-        return false;
+        return _results;
       }
-      return true;
-    });
-    return false;
-  });
+    }
+  };
 
-  $(document).on("click", "[data-screen]", function() {
-    $(this).closest('.screen, .dialog').hide();
-    return $("#" + ($(this).data('screen'))).show();
-  });
+  rooms.init();
 
-  $(".screen:not(:first-child)").hide();
-
-  DrawBlocks();
-
-  window.onresize = DrawBlocks;
+  view.init();
 
 }).call(this);
--- web/js/script.js.coffee
+++ web/js/script.js.coffee
@@ -1,87 +1,102 @@
-# Base sizes
-row_count = 4
-col_count = 6
-gutter_part = 0.04
-base_dim = 0
-gutter = 0
-
-# (Re)draw view
-DrawBlocks = () ->
-  # Get window dimentions
-  w_width = $(window).width()
-  w_height = $(window).height()
-  base_dim = Math.round(w_height/(row_count*(1+gutter_part)+gutter_part))
-  gutter = Math.round(base_dim*gutter_part)
-  #col_count = $('.column').size()+$('.column-2x').size()*2
-  # Set global sizes
-  $('body').height(w_height-gutter*2).css('font-size', "#{base_dim/2}px")
-  # Set coluns
-  container_width = (base_dim+gutter)*col_count
-  $('.container').width(container_width)
-  $('.spacer').width(w_width-container_width-gutter*2).css('margin_right', gutter).css('margin_left', gutter)
-  $('.column').width(base_dim+gutter)
-  $('.column-2x').width((base_dim+gutter)*2)
-  # Column(-1x) elements
-  $('.square').width(base_dim).height(base_dim).css('margin_bottom', gutter).css('margin_right', gutter)
-  $('.rectangle').width(base_dim).height(base_dim*2+gutter)
-  # Column-2x elements
-  $('.square-1x').width(base_dim).height(base_dim).css('margin_right', gutter)
-  $('.square-2x').width(base_dim*2+gutter).height(base_dim*2+gutter)
-  $('.rectangle-land').width(base_dim*2+gutter).height(base_dim)
-  $('.rectangle-port').height(base_dim*2+gutter).width(base_dim).css('margin_right', gutter)
-  # Margins for elements
-  $('.column, .column-2x').children().css('margin_bottom', gutter)
-
-# (Re)draw elements state and info
-PullElements = (data, status, xhr)->
-  if status == "success"
-    $.each data, (room_name, room_data)->
-      $.each room_data["elements"], (element_name, element_data)->
-        element = $("##{room_name} ##{element_name} div")
-        element.removeClass()
-        element.addClass(element_data["state"])
-        if element_data["info"]
-          element = $("##{room_name} ##{element_name} .info")
-          unless element.size() == 0
-            element.html(element_data["info"])
+view =
+  row_count: 4
+  col_count: 6
+  gutter_part: 0.04
+  init: () ->
+    # Show only first screen
+    $(".screen:not(:first-child)").hide()
+    window.onresize = () -> view.resize.call(view)
+    @resize()
+    @hook()
+  # (Re)draw view on resize
+  resize: () ->
+    # Get window dimentions
+    w_width = $(window).width()
+    w_height = $(window).height()
+    # Calculate size
+    base_dim = Math.round(w_height/(@row_count+@gutter_part*(@row_count+1)))
+    gutter = Math.round(base_dim*@gutter_part)
+    container_width = (base_dim+gutter)*@col_count
+    # Layout
+    $('body').height(w_height-gutter*2).css('font-size', "#{base_dim/2}px")
+    $('.container').width(container_width)
+    $('.spacer').width(w_width-container_width-gutter*2).css('margin_right', gutter).css('margin_left', gutter)
+    $('.column').width(base_dim+gutter)
+    $('.column-2x').width((base_dim+gutter)*2)
+    # Column(-1x) elements
+    $('.square').width(base_dim).height(base_dim).css('margin_right', gutter)
+    $('.rectangle').width(base_dim).height(base_dim*2+gutter)
+    # Column-2x elements
+    $('.square-1x').width(base_dim).height(base_dim).css('margin_right', gutter)
+    $('.square-2x').width(base_dim*2+gutter).height(base_dim*2+gutter)
+    $('.rectangle-land').width(base_dim*2+gutter).height(base_dim)
+    $('.rectangle-port').height(base_dim*2+gutter).width(base_dim).css('margin_right', gutter)
+    # All elements
+    $("[class*=square], [class*=rectangle]").css('margin_bottom', gutter)
+  rotate: (direction) ->
+    $(".screen").each () ->
+      if $(this).is(":visible")
+        $(this).hide()
+        if direction
+          next_screen = $(this).next(".screen")
+        else
+          next_screen = $(this).prev(".screen")
+        if next_screen.size() == 1
+          next_screen.show()
+        else
+          if direction
+            $(".screen").first().show()
           else
-            $("##{room_name} ##{element_name}").append("<span class='info'>#{element_data['info']}</span>")
-
-# Get initial state
-$(".screen").each ->
-  $.getJSON "#{$(this).attr("id")}/state", PullElements
-
-# Hook click events
-$(document).on "click", "[data-command]", ()->
-  screen = $(this).closest('.screen')
-  if screen.size() == 1
-    request = "/#{$(this).closest('.screen').attr('id')}"
-  else
-    request = ""
-  request += "/#{$(this).attr('id')}" if $(this).attr("id")
-  request += "/#{$(this).data('command')}"
-  $.getJSON request, PullElements
-  return false
-
-# Hook spacer's click
-$(document).on "click", ".spacer", ()->
-  $(".screen").each ()->
-    if $(this).is(":visible")
-      $(this).hide()
-      next_screen = $(this).next(".screen")
-      if next_screen.size() == 1
-        next_screen.show()
+            $(".screen").last().show()
+        return false
+      return true
+  hook: () ->
+    # Hook element's commands
+    $(document).on "click", "[data-command]", () ->
+      screen = $(this).closest('.screen')
+      if screen.size() == 1
+        request = "/#{$(this).closest('.screen').attr('id')}"
       else
-        $(".screen").first().show()
+        request = ""
+      request += "/#{$(this).attr('id')}" if $(this).attr("id")
+      request += "/#{$(this).data('command')}"
+      $.getJSON request, rooms.update
       return false
-    return true
-  return false
+    # Hook spacer
+    spacer = $(".spacer .room_switch")
+    $(document).on "click", spacer.first(), () ->
+      view.rotate(true)
+      return false
+    $(document).on "click", spacer.last(), () ->
+      view.rotate(false)
+      return false
+    # Hook screen's and dialog's links
+    $(document).on "click", "[data-screen]", () ->
+      $(this).closest('.screen, .dialog').hide()
+      $("##{$(this).data('screen')}").show()
+      return false
 
-$(document).on "click", "[data-screen]", ()->
-  $(this).closest('.screen, .dialog').hide()
-  $("##{$(this).data('screen')}").show()
+rooms =
+  # Get initial state
+  init: () ->
+    $(".screen").each ->
+      $.getJSON "#{$(this).attr("id")}/state", rooms.update
+  # Update elements state and info
+  update: (data, status, xhr) ->
+    if status == "success"
+      for room_name, room_data of data
+        for element_name, element_data of room_data.elements
+          # Add element state class
+          element = $("##{room_name} ##{element_name} div")
+          element.removeClass()
+          element.addClass(element_data.state)
+          # Add element info span
+          if element_data.info
+            element_info = element.children(".info")
+            if element_info.size() != 0
+              element_info.html(element_data.info)
+            else
+              element.append("<span class='info'>#{element_data.info}</span>")
 
-# Show only first screen
-$(".screen:not(:first-child)").hide()
-DrawBlocks()
-window.onresize = DrawBlocks
+rooms.init()
+view.init()
