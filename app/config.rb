735d932961e3a6ae9afcc384d905f08d603b98c6
Goredar <goredar@gmail.com>
2014-01-05 09:59:48 +0300

    Create config.rb

---
+++ app/config.rb
@@ -0,0 +1,62 @@
+# coding: utf-8
+module Domotics
+  CLASS_MAP = {group: :element_group}
+  class Config < BasicObject
+    def initialize(conf)
+      @current_room = {}
+      @current_device = {}
+      @groups = []
+      instance_eval(conf)
+    end
+
+    def element_group(args = {})
+      raise "Element group must have room" unless @current_room.any?
+      args[:room] = @current_room[:name]
+      unless gr = Room[@current_room[:name]][args[:name]]
+        gr = ElementGroup.new args
+        @groups[-1].add_element gr if @groups[-1]
+      end
+      @groups.push(gr)
+      yield if ::Kernel.block_given?
+      @groups.pop
+    end
+
+    def room(klass, args = {})
+      @current_room[:name] = args[:name]
+      @current_room[:type] = args[:type]
+      klass.new(args) unless Room[args[:name]]
+      yield if ::Kernel.block_given?
+      @current_room.clear
+    end
+
+    def device(klass, args = {})
+      @current_device[:name] = args[:name]
+      @current_device[:type] = args[:type]
+      klass.new(args) unless Device[args[:name]]
+      yield if ::Kernel.block_given?
+      @current_device.clear
+    end
+
+    def element(klass, args = {})
+      raise "Element must have device and room" unless @current_device.any? and @current_room.any?
+      args[:room] = @current_room[:name]
+      args[:room_type] = @current_room[:type]
+      args[:device] = @current_device[:name]
+      args[:device_type] = @current_device[:type]
+      el = klass.new(args) unless Room[@current_room[:name]][args[:name]]
+      @groups[-1].add_element el if @groups[-1]
+    end
+
+    def method_missing(symbol, *args, &block)
+      if CLASS_MAP[symbol] and name = args.shift
+        args_hash = args.shift || {}
+        args_hash[:name] = name
+        args_hash[:type] = symbol
+        args_hash[:logger] = $logger
+        __send__(*CLASS_MAP[symbol], args_hash, &block)
+      else
+        super
+      end
+    end
+  end
+end
