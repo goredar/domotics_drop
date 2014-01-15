a04ca333b8fe13c0c908cae8cd1dcf47e9246dc0
Goredar <goredar@gmail.com>
2014-01-15 08:44:34 +0200

    Update living_room.rb

--- app/room/living_room.rb
+++ app/room/living_room.rb
@@ -8,7 +8,7 @@ module Domotics
 
         board_wardrobe_light.on
       when :down
-        return board_window_light.off unless board_window_light.off
+        return board_window_light.off unless board_window_light.off?
         return long_side_light.off unless long_side_light.off?
         board_wardrobe_light.off
       end
