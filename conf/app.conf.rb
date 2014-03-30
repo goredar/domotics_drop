453dc51ee13f40b827d2a55a4a60a750831a858b
goredar <goredar@gmail.com>
2014-03-30 13:41:47 +0300

    camera

--- conf/app.conf.rb
+++ conf/app.conf.rb
@@ -1,6 +1,6 @@
 Domotics::Core::Setup.logger = Logger.new(STDERR)
 Domotics::Core::Setup.logger.level = Logger::DEBUG
-#Domotics::Core::Setup.logger.formatter = proc do |severity, datetime, progname, msg|
-#  "#{severity} #{msg}#{$/}"
-#end
+Domotics::Core::Setup.logger.formatter = proc do |severity, datetime, progname, msg|
+  "#{severity} #{msg}#{$/}"
+end
 Domotics::Core::Element.data = Domotics::Core::DataRedis.new
